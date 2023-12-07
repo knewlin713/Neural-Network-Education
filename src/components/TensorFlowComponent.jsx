1de4e101ce4a462bd50f5b52ed1cd43feb9a1b93
monoguitari <jesus_rodriguez@brown.edu>
2023-12-07 16:14:59 -0500

    Used Pre-Trained Mobilenet model to classify image
    Goal:
    1. Prediction for any user input
    2. Use custom model/train

--- src/components/TensorFlowComponent.jsx
+++ src/components/TensorFlowComponent.jsx
@@ -1,26 +1,47 @@
 import * as tf from '@tensorflow/tfjs'
 import { MnistData } from '../assets/data'
 import * as tfvis from '@tensorflow/tfjs-vis' 
+import { Box } from '@chakra-ui/react'
+import * as mobilenetmodule from '@tensorflow-models/mobilenet'
+import { useEffect } from 'react'
 
 export default function TensorFlowComponent() {
-    const data = [
-        { index: 0, value: 50 },
-        { index: 1, value: 100 },
-        { index: 2, value: 150 },
-      ];
-      
-    //   // Get a surface
-    //   const surface = tfvis.visor().surface({ name: 'Barchart', tab: 'Charts' });
-      
-    //   // Render a barchart on that surface
-    //   tfvis.render.barchart(surface, data, {});
-    // let visor=tfvis.visor();
-    // visor.isFullscreen = 1  ;
+  //1. Load model
+  // const model = mobilenetmodule.load();
+  // console.log(model);
+  let model = undefined;
+
+  useEffect(() => {
+    async function getModel() {
+      const imgPath = 'src/assets/dog.webp';
+      const img = new Image();
+      img.src = imgPath;
+      const model = await mobilenetmodule.load();
+      const prediction = model.classify(img);
+      console.log(model);
+      console.log(prediction);
+    }
+    model = getModel();
+  }, []);
+  // console.log(model);
+
+
+  //make a new Javascript image
+  
+  // tf.browser.fromPixels(img, 'float32');
+
+
+  const data = [
+      { index: 0, value: 50 },
+      { index: 1, value: 100 },
+      { index: 2, value: 150 },
+    ];
     
-      return (
-        <div>
-          
-        </div>
-      )
+
+    return (
+    <div>
+      Hello world
+    </div>
+    )
 }
 
--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -13,16 +13,17 @@ export default function Playground() {
 
             {/* <Button>Hi</Button> */}
             {/* d3.js neural network chunk */}
-            <Box maxWidth="500" borderWidth='1px' borderRadius='lg' overflow='hidden' backgroundColor="#FAF9F6">
+            {/* <Box maxWidth="500" borderWidth='1px' borderRadius='lg' overflow='hidden' backgroundColor="#FAF9F6">
                 <Heading>Visualization Method: vis.js</Heading>
                 <Text>This is the text</Text> 
                 <NeuralNetwork />
-            </Box>
+            </Box> */}
 
             {/* tensorflow attempt */}
             <Box maxWidth="500" borderWidth='1px' borderRadius='lg' overflow='hidden' maxHeight="500" backgroundColor="#FAF9F6">
                 <TensorFlowComponent/>
             </Box>
+            {/* <TensorFlowComponent /> */}
         </div>
     )
 }
