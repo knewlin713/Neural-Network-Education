79bd6bcbe0046c6b8e2dd8abe967a85ff259cd57
Kaley Newlin <kaley_newlin@brown.edu>
2023-12-14 15:52:49 -0500

    changes

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,14 +3,15 @@ import ReactPlayer from 'react-player'
 
 export default function Education() {
     return(
-        <Flex align={'center'} justify={'center'} marginTop={'75px'} direction={'column'} >
+        <Flex align={'center'} justify={'center'} marginTop={'75px'} direction={'column'} height={'560px'}>
             <Heading color='white'>Education Time</Heading>
-            <Flex justify={'space-around'}>
-                {/*<Card size='lg' backgroundColor={'#585B5B'}>
+            <Flex maxWidth={'50%'} height={'100%'}>
+                {/*<Card>
                     <CardBody>
                         hi
-                    </CardBody>             </Card>*/}
-                <ReactPlayer url="https://www.youtube.com/embed/NOEy_9DgLvU?si=ay-Iqk4YCt8Jgh32" controls/>
+                    </CardBody>
+                </Card>*/}
+                <ReactPlayer url="https://www.youtube.com/embed/NOEy_9DgLvU?si=ay-Iqk4YCt8Jgh32"/>
                 {/* <AspectRatio ratio={1} height={'100%'} width={'560px'}>
                 <video width="560" height="560" src="https://www.youtube.com/embed/NOEy_9DgLvU?si=ay-Iqk4YCt8Jgh32" title="YouTube video player" allowFullScreen/>
                 </AspectRatio> */}
