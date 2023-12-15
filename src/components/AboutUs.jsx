0474e22d6a116c3753ccb1fa0f3a499f81e8131b
monoguitari <jesus_rodriguez@brown.edu>
2023-12-15 00:33:44 -0500

    update push

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUs = forwardRef(({props}, ref) => {
         
         <Flex direction={'column'} gap={'10px'} align={'center'} justify={'center'} marginTop={'75px'}>
             <Heading color={'white'}  ref={ref}>About Us</Heading>
-            <Profile profilePicture='src/assets/thelordjesus.jpg' about = 'Hello! I am Jesus Rodriguez, a Computer Science Major at Brown University.'/>
+            <Profile profilePicture='src/assets/thelordjesus.jpg' about = 'Hello. I am Jesus Rodriguez, a Computer Science Major at Brown University.'/>
             <Profile profilePicture='src/assets/keyan.jpg'  about = 'Hey, I am Keyan Rahimi! I study Computer Science and Cognitve Nueroscience at Brown University.'/>
             <Profile profilePicture='src/assets/Kaley.jpg'  about = 'I am Kaley Newlin. Recently finding my passion for computer science and cognitve neuroscience, I hope I can work on projects like this to bring to light some misconceptions and allow for more understanding of the exciting work coming about in this new tech age.' />
             <Heading color="white">Our Mission</Heading> 
